00dbd687b4c339dd170188c2519c61a606b9665d
faiqrana <ranafaiq55@gmail.com>
2022-08-01 19:06:47 +0500

    redux toolkit added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3f464c63e6b7ef68"; }
+/******/ 		__webpack_require__.h = function() { return "1f8bd864db67be93"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Routes } from '../Utils/Routes'
+import { Provider } from 'react-redux'
+import { store } from '../store/store';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return  <Routes Component={Component} pageProps={{ ...pageProps }} />
+
+  return  (
+    <Provider store={store}>
+      <Routes Component={Component} pageProps={{ ...pageProps }} />
+    </Provider>
+  )
 }
 
 export default MyApp
