798392001fdb69117db57e7ca0d3a3e628bd4938
faiqrana <ranafaiq55@gmail.com>
2022-08-01 19:00:47 +0500

    git ignore updated

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bd167ee1f445aba1"; }
+/******/ 		__webpack_require__.h = function() { return "f654e7994db0decc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
